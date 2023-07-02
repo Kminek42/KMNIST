1c3c23dd8a7cdba6fde9a4f1747e19ca50f2489c
Kminek42 <k.kminkowski@icloud.com>
2023-07-03 01:26:24 +0200

    basic network

---
+++ training.py
@@ -0,0 +1,87 @@
+import torch
+import torch.nn as nn
+import torchvision
+from torch.utils.data import Dataset, DataLoader
+import matplotlib.pyplot as plt
+import time
+
+# get information from user
+ans = ""
+while ans != "Yes" and ans != "No":
+    ans = input("Use GPU? [Yes / No]: ")
+
+dev = torch.device("cpu")
+if ans == "Yes":
+    if torch.cuda.is_available():
+        dev = torch.device("cuda")
+    elif torch.backends.mps.is_available():
+        dev = torch.device("mps")
+
+print(f"Using {dev} device.")
+
+transform = torchvision.transforms.Compose(
+    [torchvision.transforms.ToTensor(),
+    torchvision.transforms.Normalize((0.5,), (0.5,))]
+)
+
+training_dataset = torchvision.datasets.KMNIST(
+    root="./KMNIST",
+    train=True,
+    download=True,
+    transform=transform
+)
+
+ans = ""
+while not ans.isnumeric():
+    ans = input("Enter batch size: [uint]: ")
+ans = int(ans)
+
+loader = DataLoader(
+    dataset=training_dataset,
+    batch_size=ans,
+    shuffle=True
+)
+
+# model
+input_n = 28 * 28
+
+ans = ""
+while not ans.isnumeric():
+    ans = input("Enter hidden layer size: [uint]: ")
+hidden_n = int(ans)
+output_n = 10
+activation = nn.LeakyReLU()
+
+model = nn.Sequential(
+    nn.Flatten(),
+    nn.Linear(input_n, hidden_n),
+    activation,
+    nn.Linear(hidden_n, output_n)
+).to(dev)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-2, momentum=0.9)
+
+ans = ""
+while not ans.isnumeric():
+    ans = input("Enter epoch number: [uint]: ")
+epoch_n = int(ans)
+
+t0 = time.time()
+for epoch in range(1, epoch_n + 1):
+    loss_sum = 0
+    for inputs, targets in iter(loader):
+        inputs, targets = inputs.to(dev), targets.to(dev)
+        outputs = model.forward(inputs)
+
+        optimizer.zero_grad()
+        loss = criterion(outputs, targets)
+        loss.backward()
+        optimizer.step()
+
+        loss_sum += loss
+
+    learning_time = time.time() - t0
+    remaining_time = learning_time / epoch * (epoch_n - epoch)
+    print(f"Epoch: {epoch}, mean loss: {loss_sum / len(loader)}")
+    print(f"Learning time: {time.time() - t0}, Time remaining: {remaining_time}\n")
